5fd7d28555bfcb0b300bdb6fe253590d98b54368
peaceoni <peaceoni49@yahoo.com>
2020-06-01 15:42:38 +0100

    Updates

---
+++ precache-manifest.f63ca8e927c1fbf6518a4964a1e807e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815d9cd849748d1d8a5f38625073c2c9",
+    "url": "/To-do-app-fully-functional/index.html"
+  },
+  {
+    "revision": "ea71edbef5600f03e5cd",
+    "url": "/To-do-app-fully-functional/static/css/main.7c80dc8c.chunk.css"
+  },
+  {
+    "revision": "c734dbd0ec0fb5497c3d",
+    "url": "/To-do-app-fully-functional/static/js/2.82e7e100.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-app-fully-functional/static/js/2.82e7e100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea71edbef5600f03e5cd",
+    "url": "/To-do-app-fully-functional/static/js/main.a83b00da.chunk.js"
+  },
+  {
+    "revision": "3230663286faae0c31ab",
+    "url": "/To-do-app-fully-functional/static/js/runtime-main.239742e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-do-app-fully-functional/static/media/logo.5d5d9eef.svg"
+  }
+]);
